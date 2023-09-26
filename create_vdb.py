c6490cbc5157a0ebfc3afe26084d72ef04c7f952
Shawn Cook <sfncook@gmail.com>
2023-09-26 10:28:11 -0700

    Pinecone working. Fix ignore package-lock

--- create_vdb.py
+++ create_vdb.py
@@ -5,6 +5,7 @@ from langchain.document_loaders import TextLoader, PyPDFLoader
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS, Pinecone
 import pickle
+import pinecone
 
 def get_file_chunks(filename):
     text_splitter = RecursiveCharacterTextSplitter(
@@ -23,7 +24,7 @@ def get_file_chunks(filename):
 
 def get_dir_chunks_recursively(dir_path):
     count = 0
-    ignore_list = ['.git', 'node_modules', '__pycache__', '.idea', '.vscode', '.package-lock.json', 'yarn.lock']
+    ignore_list = ['.git', 'node_modules', '__pycache__', '.idea', '.vscode', 'package-lock.json', 'yarn.lock']
     chunks = []
     for root, dirs, files in os.walk(dir_path):
         # Just for testing/debugging
@@ -43,10 +44,8 @@ def get_dir_chunks_recursively(dir_path):
                 print(f"Failed to process {filepath} due to error: {str(e)}")
     return chunks
 
-def create_vdb_from_chunks(chunks, temp_dir, repo_url):
+def create_vdb_from_chunks(chunks, temp_dir, repo_name):
     print("Creating VDB from chunks...")
-    repo_name = repo_url.split('/')[-1]
-    repo_name = clone_repo(repo_url, code_repo_path)
     vdb_path = temp_dir + "/vdb-" + repo_name + ".pkl"
     embedding = OpenAIEmbeddings(disallowed_special=())
     if os.path.exists(vdb_path):
@@ -62,18 +61,25 @@ def create_vdb_from_chunks(chunks, temp_dir, repo_url):
 
 
 
-def store_chunks_in_pinecone(chunks, pinecone_index_name):
+def store_chunks_in_pinecone(chunks, pinecone_index_name, repo_name):
     print("Storing chunks in Pinecone...")
     embeddings = OpenAIEmbeddings(disallowed_special=())
-    docsearch = Pinecone.from_documents(chunks, embeddings, index_name=pinecone_index_name)
+    pinecone_index = pinecone.Index(pinecone_index_name)
+    pinecone_vdb = Pinecone(pinecone_index, embeddings.embed_query, repo_name)
+    pinecone_vdb.add_documents(chunks)
+#     docsearch = Pinecone.from_documents(chunks, embeddings, index_name=pinecone_index_name)
     print("Done storing chunks in Pinecone!")
 
 def create_vdb(repo_url, code_repo_path, temp_dir, pinecone_index_name):
+    repo_name = repo_url.split('/')[-1]
+
+    clone_repo(repo_url, code_repo_path)
+
     chunks = get_dir_chunks_recursively(code_repo_path)
 
     # Pick one:
-#     create_vdb_from_chunks(chunks, temp_dir, repo_url)
-    store_chunks_in_pinecone(chunks, pinecone_index_name)
+#     create_vdb_from_chunks(chunks, temp_dir, repo_name)
+    store_chunks_in_pinecone(chunks, pinecone_index_name, repo_name)
 
     print("VDB generated!")
     return True
--- query_vdb.py
+++ query_vdb.py
@@ -4,9 +4,8 @@ from langchain.vectorstores import FAISS, Pinecone
 def get_contexts_from_pinecone(query):
 
 
-def query_vdb(query, pinecone_api_key, pinecone_environment, pinecone_index_name):
+def query_vdb(query):
     print("Querying VDB...")
-    pinecone.init(api_key=pinecone_api_key, environment=pinecone_environment)
     matched_docs = Pinecone.similarity_search(query, k=10)
     output = ""
     for idx, docs in enumerate(matched_docs):
